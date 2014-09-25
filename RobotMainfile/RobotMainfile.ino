ae7f12ca84ab601d52bbf765c65d2a10992ed5a5
lkirman <lkirman@stevens.edu>
2014-09-25 16:23:49 -0400

    Mainfile
    We update this later

---
+++ RobotMainfile/RobotMainfile.ino
@@ -0,0 +1,16 @@
+#include <SendOnlySoftwareSerial.h>
+#include <ArduinoInit.h>
+  
+void setup()
+{
+  // put your setup code here, to run once:
+  configArduino();
+  
+}
+
+void loop()
+{
+  
+  
+}
+
